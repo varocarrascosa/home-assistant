cd0a82aa74608df0d171f678731fd84e1247f2ab
varocarrascosa <Alvaro.carrascosa@hotmail.com>
2017-01-15 13:06:16 -0500

    Add files via upload

--- cmds/pullconfig.sh
+++ cmds/pullconfig.sh
@@ -1,7 +1,7 @@
-!/bin/bash
+#!/bin/bash
 
 mkdir /home/homeassistant/.homeassistant/temp
 cd /home/homeassistant/.homeassistant/temp/
 git clone https://github.com/varocarrascosa/home-assistant.git
-mv /home/homeassistant/.homeassistant/temp/home-assistant/* /home/homea$
+cp -r /home/homeassistant/.homeassistant/temp/home-assistant/* /home/homeassistant/.homeassistant/
 rm -rf /home/homeassistant/.homeassistant/temp
