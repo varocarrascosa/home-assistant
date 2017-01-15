dd6a3d0b01b35001a4ba749579cf1fb7bfa25b56
varocarrascosa <Alvaro.carrascosa@hotmail.com>
2017-01-15 04:44:31 -0500

    Add files via upload

---
+++ cmds/dominos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/home/tweet.sh/tweet.sh post "@dominos #EASYORDER"
+sleep 4m
+/home/tweet.sh/tweet.sh dm @dominos CONFIRM
+
+
+
---
+++ cmds/pullconfig.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+
+mkdir /home/homeassistant/.homeassistant/temp
+cd /home/homeassistant/.homeassistant/temp/
+git clone https://github.com/varocarrascosa/home-assistant.git
+mv /home/homeassistant/.homeassistant/temp/home-assistant/* /home/homea$
+rm -rf /home/homeassistant/.homeassistant/temp
