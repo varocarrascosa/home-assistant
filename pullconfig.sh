b7108a24cdea115b1745be4dd89d5320abc66164
varocarrascosa <Alvaro.carrascosa@hotmail.com>
2018-01-23 22:21:42 -0500

    Delete pullconfig.sh

--- pullconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir /home/pi/.homeassistant/temp/
-cd /home/pi/.homeassistant/temp/
-git clone https://github.com/varocarrascosa/home-assistant.git
-cp -r /home/pi/.homeassistant/temp/home-assistant/* /home/pi/.homeassistant/
-rm -rf /home/pi/.homeassistant/temp
